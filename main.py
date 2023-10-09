88571a397655d9fb9319c4266fa30a62d1a7ee7c
jevnik <lukajevnik@gmail.com>
2023-10-09 18:11:56 +0200

    game logic and functionality now complete and bug free to my knowlage

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 
 level = 2
+game_over = False
 
 class MenuScreen(Screen):
     def start_game(self):
@@ -21,6 +22,7 @@ class MenuScreen(Screen):
         game_over_screen = app.root.get_screen("GameOverScreen")
         game_over_screen.game_restart()
         app.root.current = "MainScreen"
+        game_over = False
 
 class LevelCompletedScreen(Screen):
     def back(self,event):
@@ -35,37 +37,44 @@ class LevelCompletedScreen(Screen):
 
 class GameOverScreen(Screen):
     def game_restart(self):
+        global game_over
         MainScreen.gen_num = 0
         MainScreen.gen_num_clicked = False
         app = App.get_running_app()
         main_screen = app.root.get_screen("MainScreen")
+        
         for button in main_screen.ids.scrollable_buttons.buttons[1:]:
             button.stored_num = None
             button.text = button.old_text
             button.disabled = False
                        
-
+        MainScreen.solve_num = 0
+        game_over = False
         app.root.current = "MenuScreen"
+        
 
 
 class MainScreen(Screen):
     gen_num_label = StringProperty("Generate first number to start")
     gen_num = 0
     gen_num_clicked = False
-    
+    solved_num = 0
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         
     def rand_butt_press(self,button):
         if not self.gen_num_clicked:
-            print("button pressed")
             MainScreen.gen_num = random.randint(1,100)
             MainScreen.gen_num_clicked = True
             self.gen_num_label = f"{MainScreen.gen_num}"
             print(self.gen_num_label)
     
+    def reload(self):
+        for button in self.ids.scrollable_buttons.buttons[1:]:
+            self.ids.scrollable_buttons.remove_widget(button)
+        self.ids.scrollable_buttons.add_buttons_for_level(level)
+        print(self.ids.scrollable_buttons.buttons)
 
-        
 class GameArea(BoxLayout):
     def __init__(self, **kwargs):
         super(GameArea, self).__init__(**kwargs)
@@ -77,6 +86,12 @@ class ScrollableButtons(BoxLayout):
         self.orientation = "vertical"
         self.spacing = dp(10)
         self.buttons = [None,]
+
+    
+    def add_buttons_for_level(self,level):
+
+        self.clear_widgets()
+        self.buttons = [None,]
         for i in range(level):
             old_text = f" {i+1}. Number"
             button = Button(text=old_text,size_hint_y=None, height=100)
@@ -86,11 +101,15 @@ class ScrollableButtons(BoxLayout):
             button.stored_num = None
             self.buttons.append(button)
             self.add_widget(button)
-
+        
+    
+    
     def entered_value(self,button):
+        solved_num = 0
+        global level
+        global game_over
         if MainScreen.gen_num_clicked:  
             button.text = str(MainScreen.gen_num)
-            print(button.id)
             button.disabled = True
             button.stored_num = MainScreen.gen_num
             MainScreen.gen_num = 0
@@ -99,37 +118,36 @@ class ScrollableButtons(BoxLayout):
         for i in range(1,int(button.id[1:])): #checking game over if some number before stored number is bigger
             if self.buttons[i].stored_num:
                 if self.buttons[i].stored_num > button.stored_num:
-                    print("Game over")
+                    game_over = True
+                    
                     app = App.get_running_app()
                     app.root.current = "GameOverScreen"
                     break
 
-        
         for i in range(int(button.id[1:]),len(self.buttons)): #checking game over if some number after stored number is smaller
             if self.buttons[i].stored_num:
                 if self.buttons[i].stored_num < button.stored_num:
-                    print("Game over")
+                    game_over = True
+                    
                     app = App.get_running_app()
                     app.root.current = "GameOverScreen"
                     break
         
-        #checking for victory
-        solved_num = 0
-        for button in self.buttons[1:]: 
-            if not button.stored_num:
-                break
+        
+        if not game_over:
+            for button in self.buttons[1:]: 
+                if not button.stored_num:
+                    break
+                else:
+                    solved_num += 1
+
+            if solved_num == len(self.buttons)-1:
+                app = App.get_running_app()
+                app.root.current = "LevelCompletedScreen"
+                level += 1
+                print(level)
             else:
-                solved_num += 1
-
-        if solved_num == len(self.buttons)-1:
-            app = App.get_running_app()
-            app.root.current = "LevelCompletedScreen"
-            global level
-            level += 1
-            print(level)
-        else:
-            solved_num = 0
-
+                solved_num = 0
 
 
 class ScreenManager(ScreenManager):
@@ -141,6 +159,5 @@ class MyApp(App):
     def build(self):
         return kv
     
-
 if __name__ == '__main__':
     MyApp().run()
